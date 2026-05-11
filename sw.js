03aafdde860422c0f2eb2567ae9757966454a093
silverstratter <thesilverstratter@gmail.com>
2026-05-11 01:50:07 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'aethrys-mobile-v1';
+const CACHE = 'aethrys-mobile-v2';
 const ASSETS = [
   './', './index.html', './style.css', './app.js', './manifest.json', './icon-192.svg', './icon-512.svg',
   './characters_player.json', './relationships_player.json', './houses.json', './factions.json', './regions.json'
